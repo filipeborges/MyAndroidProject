4e2cffe88fbfe5014cc9d9f89dc0ff0dd6fe9c9e
Filipe Borges <filipe_bkc@yahoo.com.br>
2014-12-31 17:52:43 -0200

    Implemented 'getScreenProperties' method.

--- src/com/mycustomlibs/ScreenUtilities.java
+++ src/com/mycustomlibs/ScreenUtilities.java
@@ -1,7 +1,26 @@
 package com.mycustomlibs;
 
+import android.util.DisplayMetrics;
+import android.content.Context;
+import android.view.WindowManager;
+
 public class ScreenUtilities {
 
+	public static int[] getScreenProperties(Context context) {
+		if(context == null) {
+			return null;
+		}
+		else {
+			DisplayMetrics metrics = new DisplayMetrics();
+			((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metrics);
+			int[] screenProperties = new int[3];
+			screenProperties[0] = metrics.densityDpi;
+			screenProperties[1] = metrics.widthPixels;
+			screenProperties[2] = metrics.heightPixels;
+			return screenProperties;
+		}
+	}
+	
 	public static int getPxFromDp(int dpUnits, int screenDensity) {
 		if(dpUnits <= 0 || screenDensity <= 0) {
 			return -1;
