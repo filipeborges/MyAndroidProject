2cc77120d6b3805c8b9be4bf854310b9eea262ce
Filipe Borges <filipe_bkc@yahoo.com.br>
2014-12-31 16:38:46 -0200

    Refactoring 'getPxFromDp' method.

--- src/com/mycustomlibs/ScreenUtilities.java
+++ src/com/mycustomlibs/ScreenUtilities.java
@@ -2,12 +2,13 @@ package com.mycustomlibs;
 
 public class ScreenUtilities {
 
-	public int getPxFromDp(int dpUnits, int screenDensity) {
+	public static int getPxFromDp(int dpUnits, int screenDensity) {
 		if(dpUnits <= 0 || screenDensity <= 0) {
 			return -1;
 		}
 		else {
-			return dpUnits * (screenDensity/160);
+			float result = dpUnits * ((float)screenDensity/(float)160);
+			return (int)result;
 		}
 	}
 }
